dcc1a70367013494c2bc681db46161e1438d3d72
Sunken Ahn <devsanso@outlook.com>
2022-06-29 01:36:03 +0900

    불필요 코드 삭제

--- src/api/body/select.ts
+++ src/api/body/select.ts
@@ -2,12 +2,6 @@ export interface UserRequestBody {
     userId : string
 }
 
-
-export interface PlaceRequestBody {
-    placeId : string
-}
-
-
 export interface ReviewRequestBody {
     reviewId : string
 }
