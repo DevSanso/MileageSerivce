670f8edcf662796e8b04f9755524adcf7bd34a6f
Sunken Ahn <devsanso@outlook.com>
2022-06-28 22:42:25 +0900

    라우터 내용 작성중, 아직 미완성, 포인트 조회 api 작성 예정

--- src/api/event-controller.ts
+++ src/api/event-controller.ts
@@ -1,13 +1,52 @@
+import {Context} from 'koa';
 import Router from 'koa-router';
 
+
 import BodyType from './body/event';
+
+import createReviewService from '../services/db/create_review';
+import selectUserPointService from '../services/db/select_user_point';
+import createUserPointService from '../services/db/create_user_point';
+
+import {convertCtxType} from '../utils/koa/convert_ctx_type';
+import {checkProps} from '../utils/json/check_props';
+import {ErrorObject,ErrorType} from '../middleware/type/error-object';
+
+
+
 const controller = new Router();
 
 
 
-controller.post("review-event","/events",(ctx,next)=> {
+const addReviewHandle = async (ctx : Context)=> {
+    await createReviewService(ctx);
+    const exist = await selectUserPointService(ctx)
+    if (exist == null)await createUserPointService(ctx); 
+}
+
+const checkP = (body : any) => checkProps<BodyType>(body,
+    ["action","attachedPhotoIds","content","placeId","reviewId","type","userId"]);
+
+controller.post("review-event","/events",async (ctx,next)=> {
     const requestBody : BodyType = ctx.request.body;
-    
+
+    if(!checkP(requestBody))
+        throw new ErrorObject(ErrorType.Request,400,`bad request body : ${JSON.stringify(requestBody)}`);
+
+    const convertCtx = convertCtxType(ctx);
+
+    if(requestBody.action == "ADD") {
+        addReviewHandle(convertCtx);
+        ctx.status = 200;
+        ctx.body = "Ok";
+    }
+    else if(requestBody.action == "MOD" || requestBody.action == "DELETE") {
+        throw new ErrorObject(ErrorType.Request,400,`not implement action handle : ${requestBody.action}`);
+    }
+    else {
+        throw new ErrorObject(ErrorType.Request,400,`not allow this action : ${requestBody.action}`);
+    }
+    next();
 }) ;
 
 
--- src/api/select-controller.ts
+++ src/api/select-controller.ts
@@ -1,20 +1,39 @@
 import Router from 'koa-router';
 
-import {UserRequestBody,PlaceRequestBody} from './body/select';
+
+import selectUserPointService from '../services/db/select_user_point';
+
+import {UserRequestBody} from './body/select';
+import {convertCtxType} from '../utils/koa/convert_ctx_type';
+import {checkProps} from '../utils/json/check_props';
+import {ErrorObject,ErrorType} from '../middleware/type/error-object';
+
 
 const controller = new Router();
 
-controller.post("select-point","/point/user",(ctx,next)=> {
-    const requestBody : UserRequestBody = ctx.request.body;
+controller.post("select-user-point","/point/user",async (ctx,next)=> {
+    const body = ctx.request.body as UserRequestBody;
+    if(!checkProps<UserRequestBody>(body,["userId"])) 
+        throw new ErrorObject(ErrorType.Request,400,`bad request body : ${JSON.stringify(body)}`);
+    
+    const convertCtx = convertCtxType(ctx);
+    const res = await selectUserPointService(convertCtx);
+
+    if(res == null) 
+        throw new ErrorObject(ErrorType.Request,400,`not exist user data : ${body.userId}`);
+    
+    ctx.status = 200;
+    ctx.body = JSON.stringify(res);
+    next();
 });
 
 controller.post("select-point","/point/place",(ctx,next)=> {
-    const requestBody : PlaceRequestBody = ctx.request.body;
-}) ;
+   
+});
 
 
 controller.post("select-point","/point/review",(ctx,next)=> {
-    const requestBody : PlaceRequestBody = ctx.request.body;
+    
 }) ;
 
 export default controller;
