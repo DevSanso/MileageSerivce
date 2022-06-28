dff2c045dabc91e2b5a0f13040e42137d4a6f38a
Sunken Ahn <devsanso@outlook.com>
2022-06-28 13:02:03 +0900

    유저 포인터 모델

---
+++ src/dtfp/user_point.ts
@@ -0,0 +1,16 @@
+import { RowDataPacket } from "mysql2/promise";
+import UserPoint from '../models/user_point';
+
+type DTO = {
+    point_score : number
+};
+
+const transDaoToModel = (row : RowDataPacket) : UserPoint => {
+    const dto = row as DTO;
+    return {
+        score : dto.point_score
+    }; 
+};
+
+
+export default transDaoToModel;
---
+++ src/models/user_point.ts
@@ -0,0 +1,5 @@
+
+
+export default interface UserPoint {
+    score : number
+}
