3e0bd7253c4fa5f69a872bb95e5816e432461931
Sunken Ahn <devsanso@outlook.com>
2022-06-28 19:19:26 +0900

    리뷰 after insert 트리거 생성 : 로그 데이터 생성 기능

--- assets/sql/trigger.sql
+++ assets/sql/trigger.sql
@@ -8,13 +8,19 @@ CREATE TRIGGER delete_review_before
         END$$
 DELIMITER ;
 
-
+DELIMITER $$
+CREATE TRIGGER insert_review_after
+    AFTER INSERT ON review FOR EACH ROW
+        BEGIN
+            INSERT INTO point_increase_log(review_id,place_id,log_date) VALUES (NEW.review_id,NEW.place_id,NOW());
+        END$$
+DELIMITER ;
 
 DELIMITER $$
 CREATE TRIGGER delete_review_after
     AFTER DELETE ON review FOR EACH ROW
         BEGIN
-            INSERT INTO review_deleted_log(review_id,place_id)
+            INSERT INTO point_deleted_log(review_id,place_id)
             SELECT review_id,place_id 
             FROM point_increase_log WHERE review_id = OLD.review_id;
 
