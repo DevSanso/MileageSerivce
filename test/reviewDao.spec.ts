0ae4ab1aeb4cc9b6947f23f833cffd9059577c31
Sunken Ahn <devsanso@outlook.com>
2022-06-28 22:40:08 +0900

    테스트 후 테이블 튜플 삭제

--- test/reviewDao.spec.ts
+++ test/reviewDao.spec.ts
@@ -3,8 +3,20 @@ import {expect} from 'chai';
 import dbPoolGen from "./util/db";
 import ReviewDao from "../src/dao/review";
 import {uuid} from 'uuidv4';
+import { PoolConnection } from 'mysql2/promise';
+import Review from '../src/models/review';
+import ReviewContent from '../src/models/review_content';
+import ReviewPointFlag from '../src/models/review_point_flag';
 
-describe("ReviewDao Object Test",async ()=>{
+
+const deleteAllData = async (conn : PoolConnection) => {
+    await conn.execute("DELETE FROM review;");
+    conn.execute("DELETE FROM point_increase_log;");
+    conn.execute("DELETE FROM point_deleted_log;");
+    conn.execute("DELETE FROM user_point;");
+}
+
+describe("ReviewDao Object Test",()=>{
     const pool = dbPoolGen();
     const conn =  pool.getConnection();
 
@@ -27,19 +39,67 @@ describe("ReviewDao Object Test",async ()=>{
         await dao.createReviewPointFlag(reviewId);
     });
    it("dao select review test",async() => {
-        const review = await dao.selectReview(reviewId);
+        const review = await dao.selectReview(reviewId) as Review;
         expect(review.comment).to.equal(comment);
         expect(review.userId).to.equal(userId);
         expect(review.plcaeId).to.equal(placeId);
+        const review1 = await dao.selectReview("");
+        expect(review1).to.equal(null);
    });
 
    it("dao select review content test",async() => {
-        const reviewContent = await dao.selectReviewContent(reviewId);
+        const reviewContent = await dao.selectReviewContent(reviewId) as Array<ReviewContent>;
+        for(let i = 0;i<reviewContent.length;i++) {
+            const str = imageIds.find(value => value == reviewContent[i].imageId);
+            expect(str).to.not.equal(undefined);
+        }
     });
     it("dao select review point flag test",async() => {
-        const reviewFlag = await dao.selectReviewPointFlag(reviewId);
+        const reviewFlag = await dao.selectReviewPointFlag(reviewId) as ReviewPointFlag;
         expect(reviewFlag.isTextWrite).to.equal(false);
         expect(reviewFlag.isFirstReview).to.equal(false);
         expect(reviewFlag.isUpdateImage).to.equal(false);
+    });
+
+
+    
+
+    
+    it("dao 첫번째 리뷰 관련 처리 test",async() => {
+
+        const createOtherFlag = async (review_id : string,user_id : string) => {
+            await dao.insertReview({
+                reviewId : review_id,
+                placeId : placeId,
+                userId : user_id,
+                content : comment
+            });
+            await dao.createReviewPointFlag(review_id);
+        }
+
+        await dao.updateReviewPoint(reviewId,placeId);
+        let reviewFlag = await dao.selectReviewPointFlag(reviewId) as ReviewPointFlag;
+        expect(reviewFlag.isFirstReview).to.equal(true);
+        const secondReviewId = uuid();
+        const secondUserId = uuid();
+        const thirdReviewId = uuid();
+        const thirdUserId = uuid();
+
+        await createOtherFlag(secondReviewId,secondUserId);
+        await dao.updateReviewPoint(secondReviewId,placeId);
+        reviewFlag = await dao.selectReviewPointFlag(secondReviewId) as ReviewPointFlag;
+        expect(reviewFlag.isFirstReview).to.equal(false);
+
+        await dao.deleteReview(reviewId);
+
+        await createOtherFlag(thirdReviewId,thirdUserId);
+        await dao.updateReviewPoint(thirdReviewId,placeId);
+        reviewFlag = await dao.selectReviewPointFlag(thirdReviewId) as ReviewPointFlag;
+        expect(reviewFlag.isFirstReview).to.equal(true);
+    });
+
+
+    it("모든 테이블 튜플 삭제 ",async() => {
+        await deleteAllData(await conn);
     });
 });
