c3920e52345fe6fc1d3ba98ddf1f563e88bc8db6
Sunken Ahn <devsanso@outlook.com>
2022-06-28 17:15:35 +0900

    insert 매개변수 속성값 변경 , 문자열 값들은 따음표로 감쌈,일부 오타 수정, delete 함수 추가

--- src/dao/review.ts
+++ src/dao/review.ts
@@ -11,30 +11,34 @@ import transReviewContent from '../dtfp/review_content';
 import transReview from '../dtfp/review';
 
 
-type InsertParam = Omit<Body,"action" | "type">;
+type InsertParam = Omit<Body,"action" | "type" | "attachedPhotoIds" | "content"> & {content : string | null};
 
 const selectReviewPointFlagQuery = (reviewId : string) => 
-"SELECT is_text_write, is_update_image,is_first_review" +
- `FROM review_point_flag WHERE review_id=${reviewId} LIMIT 1;`;
+"SELECT is_text_write, is_update_image,is_first_review " +
+ `FROM review_point_flag WHERE review_id="${reviewId}" LIMIT 1 ;`;
 
 
  const selectReviewContentQuery= (reviewId : string) =>  
-`SELECT image_id FROM review_content WHERE review_id=${reviewId};`;
+`SELECT image_id FROM review_content WHERE review_id="${reviewId}";`;
 
 const selectReviewQuery = (reviewId : string) => 
-`SELECT user_id,place_id,comment FROM review WHERE review_id=${reviewId};`;
+`SELECT user_id,place_id,comment FROM review WHERE review_id="${reviewId}";`;
 
-const updateReviewPointTextFlagQuery = (review_id : string) =>`update_review_point_text_flag_proc(${review_id});`;
-const updateReviewPointImageFlagQuery = (review_id : string) =>`update_review_point_image_flag_proc(${review_id});`;
+const updateReviewPointTextFlagQuery = (review_id : string) =>`update_review_point_text_flag_proc("${review_id}");`;
+const updateReviewPointImageFlagQuery = (review_id : string) =>`update_review_point_image_flag_proc("${review_id}");`;
 const updateReviewPointFirstReviewFlagQuery = (review_id : string,place_id : string) =>
-`update_review_point_first_review_flag_proc(${review_id},${place_id});`;
+`update_review_point_first_review_flag_proc("${review_id}","${place_id}");`;
 
-const insertReviewQuery = (review_id : string,user_id : string,place_id : string,comment : string) =>
-`INSERT INTO review(review_id,user_id,place_id,comment) VALUES(${review_id},${user_id},${place_id},${comment});`;
+const insertReviewQuery = (review_id : string,user_id : string,place_id : string,comment : string | null) =>
+"INSERT INTO review(review_id,user_id,place_id,comment) "+
+`VALUES("${review_id}","${user_id}","${place_id}",${comment});`;
 
 
 const insertReviewCotentQuery = (review_id : string,image_id : string) =>
-`INSERT INTO review_content(review_id,image_id) VALUES(${review_id},${image_id});`;
+`INSERT INTO review_content(review_id,image_id) VALUES("${review_id}","${image_id}");`;
+
+const deleteReivewQuery = (review_id : string) =>
+`DELETE FROM review WHERE review_id = "${review_id}";`;
 
 class ReviewDao {
     private connPromise : Promise<mysql.PoolConnection>;
@@ -76,7 +80,8 @@ class ReviewDao {
 
     public insertReview = async (body : InsertParam) => {
         const conn = await this.connPromise;
-        await conn.execute(insertReviewQuery(body.reviewId,body.userId,body.placeId,body.content));
+        const content = body.content != null ? `"${body.content}"` : null;
+        await conn.execute(insertReviewQuery(body.reviewId,body.userId,body.placeId,content));
     };
 
     public insertReviewContent = async (reviewId : string,imgId : string) => {
@@ -85,7 +90,11 @@ class ReviewDao {
     }
     public createReviewPointFlag = async (reviewId : string) => {
         const conn = await this.connPromise;
-        await conn.execute(`INSERT INTO review_point_flag VALUES(${reviewId},0,0,0);`);
+        await conn.execute(`INSERT INTO review_point_flag VALUES("${reviewId}",0,0,0);`);
+    }
+    public deleteReview = async (reviewId : string) => {
+        const conn = await this.connPromise;
+        await conn.execute(deleteReivewQuery(reviewId));
     }
 }
 
