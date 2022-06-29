ef327012c7f4073d9e363d43ef38e23f90bf10d4
Sunken Ahn <devsanso@outlook.com>
2022-06-29 12:35:16 +0900

    각 플래그들을 데이터베이스의 tinyint 타입에 맞게 정수형으로 변환

--- src/dao/log.ts
+++ src/dao/log.ts
@@ -1,8 +1,11 @@
 import mysql from 'mysql2/promise';
 
 
-const insertPushPointQuery = (reviewId : string,writeFlag : boolean,imgFlag : boolean,firstFlag : boolean) =>
+const insertPushPointQuery = (reviewId : string,writeFlag : number,imgFlag : number,firstFlag : number) =>
 `INSERT INTO point_plus_log VALUES(${reviewId},${writeFlag},${imgFlag},${firstFlag},NOW());`;
+
+
+const convertNumber = (flag : boolean) => flag? 1 : 0;
 
 class LogDao {
     private connPromise : Promise<mysql.PoolConnection>;
@@ -12,7 +15,8 @@ class LogDao {
     
     public insertPushPointIog = async(reviewId : string,writeFlag : boolean,imgFlag : boolean,firstFlag : boolean) => {
         const conn = await this.connPromise;
-        await conn.execute(insertPushPointQuery(reviewId,writeFlag,imgFlag,firstFlag));
+        const c = convertNumber;
+        await conn.execute(insertPushPointQuery(reviewId,c(writeFlag),c(imgFlag),c(firstFlag)));
     }
     
 }
