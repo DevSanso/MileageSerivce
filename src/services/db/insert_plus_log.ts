2d7b9387578f6188b9b49d1a3f8591d77d220767
Sunken Ahn <devsanso@outlook.com>
2022-06-29 23:35:41 +0900

    누락한 await 키워드 추가

--- src/services/db/insert_plus_log.ts
+++ src/services/db/insert_plus_log.ts
@@ -9,7 +9,7 @@ import ReviewPointFlag from '../../models/review_point_flag'
 
 const  insertPointPlusLogService = async (ctx : ExtendContext,reviewId : string,flags : ReviewPointFlag) => {
     const dao =  await ctx.daoProvider.log();
-    dao.insertPushPointIog(reviewId,flags.isTextWrite,flags.isUpdateImage,flags.isFirstReview);
+    await dao.insertPushPointIog(reviewId,flags.isTextWrite,flags.isUpdateImage,flags.isFirstReview);
 }
 
 export default insertPointPlusLogService;
