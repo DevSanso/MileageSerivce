b7c07df0bafed24dec7caa025c9bf2e9562db7ee
Sunken Ahn <devsanso@outlook.com>
2022-06-29 23:35:05 +0900

    insert 문자열 리터럴 추가, selectall추가

--- src/dao/log.ts
+++ src/dao/log.ts
@@ -1,8 +1,13 @@
-import mysql from 'mysql2/promise';
+import mysql,{RowDataPacket,FieldPacket} from 'mysql2/promise';
+
+
+import PointPlusLogModel from '../models/point_plus_log';
+import transPointPlusLog from '../dtfp/point_plus_log';
 
 
 const insertPushPointQuery = (reviewId : string,writeFlag : number,imgFlag : number,firstFlag : number) =>
-`INSERT INTO point_plus_log VALUES(${reviewId},${writeFlag},${imgFlag},${firstFlag},NOW());`;
+`INSERT INTO point_plus_log VALUES("${reviewId}",${writeFlag},${imgFlag},${firstFlag},NOW());`;
+
 
 
 const convertNumber = (flag : boolean) => flag? 1 : 0;
@@ -17,6 +22,16 @@ class LogDao {
         const conn = await this.connPromise;
         const c = convertNumber;
         await conn.execute(insertPushPointQuery(reviewId,c(writeFlag),c(imgFlag),c(firstFlag)));
+    }
+
+    public selectAllLog = async() : Promise<Array<PointPlusLogModel>> => {
+        const q = "SELECT review_id, text_write, update_image, first_review, log_date FROM point_plus_log " +
+            "ORDER BY log_date ASC;";
+        const conn = await this.connPromise;
+        const [rows,field] : [RowDataPacket[],FieldPacket[]] =
+            await conn.query(q);
+        
+        return rows.map(value => transPointPlusLog(value));
     }
     
 }
