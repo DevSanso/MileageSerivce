aee2c0f65b1c6c6050f2ffe7e3156ac1012876c9
Sunken Ahn <devsanso@outlook.com>
2022-06-30 20:55:20 +0900

    await키워드 추가

--- test/util/deleteAll.ts
+++ test/util/deleteAll.ts
@@ -6,10 +6,11 @@ import mysql from 'mysql2/promise';
 
 const deleteAllData = async (conn : mysql.PoolConnection) => {
     await conn.execute("DELETE FROM review;");
-    conn.execute("DELETE FROM review_create_log;");
-    conn.execute("DELETE FROM review_deleted_log;");
-    conn.execute("DELETE FROM user_point;");
-    conn.execute("DELETE FROM point_plus_log;");
+    await conn.execute("DELETE FROM review_create_log;");
+    await conn.execute("DELETE FROM review_deleted_log;");
+    await conn.execute("DELETE FROM user_point;");
+    await conn.execute("DELETE FROM point_plus_log;");
+    await conn.execute("DELETE FROM review_content;");
 }
 
 
