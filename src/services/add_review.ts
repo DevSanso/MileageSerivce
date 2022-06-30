848b2fdbf40c2d5a1896df09d4524f82278bc091
Sunken Ahn <devsanso@outlook.com>
2022-06-30 16:01:39 +0900

    수정, 생성된 서비스 소스코드에 맞쳐 변경

--- src/services/add_review.ts
+++ src/services/add_review.ts
@@ -3,13 +3,17 @@ import {ExtendContext} from '../utils/extend/koa/context';
 import RequestBody from '../api/body/event';
 import ReviewPointFlag from '../models/review_point_flag';
 
+
+
 import createReviewService from './db/create_review';
 import createUserPointService from './db/create_user_point';
 import selectUserPointService from './db/select_user_point';
 import updateReviewPointFlagService from './db/update_review_point_flag';
 import selectReviewPointFlagService from './db/select_review_point_flag';
 import insertPointPlusLogService from './db/insert_plus_log';
-import updateUserPointService from './db/update_user_point';
+import selectReviewService from './db/select_review';
+import updateReviewCommentService from './db/update_review_comment';
+import insertImagesService from './db/insert_images';
 
 type ServiceArgsType = Omit<RequestBody,"action" | "type" >;
 
@@ -20,14 +24,41 @@ const afterTreatment = async (ctx : ExtendContext,args : ServiceArgsType) => {
         await insertPointPlusLogService(ctx,args.reviewId,flag);
 }
 
+
+const chkProps = <P extends keyof ServiceArgsType>(args : ServiceArgsType, props : P) => 
+    args[props] != undefined;
+
+
 const addReviewService = async (ctx : ExtendContext,args : ServiceArgsType) => {
     const conn = await ctx.dbPoolConn;
+   
     try {
+        const existReview = selectReviewService(ctx,args.reviewId);
+        const existUserPoint = selectUserPointService(ctx,args.userId);
+
+        if((await existReview) == null) 
+            await createReviewService(ctx,args);
+        if((await existUserPoint) == null)
+            await createUserPointService(ctx,args.userId); 
+
         await conn.beginTransaction();
-        await createReviewService(ctx,args);
+
+        let updateCommentPromise : Promise<void> | null = null;
+        let insertImagesPromise : Promise<void> | null = null;
+        
+        if(chkProps(args,"attachedPhotoIds")) {
+            insertImagesPromise = insertImagesService(ctx,args.reviewId,args.attachedPhotoIds as Array<string>);
+        }
+        if(chkProps(args,"content")) {
+            updateCommentPromise = updateReviewCommentService(ctx,args.reviewId,
+                args.content != undefined?args.content : null);
+        }
+ 
+        
+        await updateCommentPromise;
+        await insertImagesPromise;
+
         await updateReviewPointFlagService(ctx,args.reviewId,args.placeId);
-        const exist = await selectUserPointService(ctx,args.userId);
-        if (exist == null)await createUserPointService(ctx,args.userId); 
         await afterTreatment(ctx,args);
         
         await conn.commit();
