95e365df468cda0d49f2a83566f4f5d16a114dd0
Sunken Ahn <devsanso@outlook.com>
2022-06-30 22:32:21 +0900

    events MOD ,DELETE 액션 처리 테스트

--- test/httpTest/server.spec.ts
+++ test/httpTest/server.spec.ts
@@ -1,5 +1,5 @@
 import http from 'http';
-
+import {uuid} from 'uuidv4';
 import axios from 'axios';
 const dbConfig = require( '../../assets/test/dbConfig.json');
 
@@ -14,7 +14,7 @@ import App from '../../src/app';
 
 
 
-import {makeRandomEventBody} from '../util/mock/body';
+import {makeBasicRandomEventBody, makeRandomEventBody} from '../util/mock/body';
 import {onlyDaoContext}  from '../util/mock/context';
 import deleteAllData from '../util/deleteAll';
 import dbPoolGen from "../util/db";
@@ -35,20 +35,38 @@ describe("http 서버 테스트",()=>{
     
     describe("Post 처리 Test",() => {
         const request = makeRandomEventBody("hello world");
-        it("/events 처리 Test",async () => {
-
+        it("/events Add 처리 Test",async () => {
             const res = await axios.post("http://localhost:3000/events",request);
-            expect(res.status).to.equal(200);
+            expect(res.status).to.equal(201);
             const dbTuple = onlyDaoCtx.daoProvider.review().selectReview(request.reviewId);
             expect(dbTuple).to.not.equal(null);
         })
 
-        it("/events mod 액션 이미지 수정시 에러 검출 Test",async () => {
-            request.action = "MOD";
-            const res = await axios.post("http://localhost:3000/events",request,{validateStatus : () => true});
-            expect(res.status).to.equal(400);
+        it("/events mod 액션 이미지 수정 Test",async () => {
 
+            const basic = makeBasicRandomEventBody("MOD") as any;
+            basic.reviewId = request.reviewId;
+            basic.placeId = request.placeId;
+            basic.userId = request.userId;
+
+            basic["attachedPhotoIds"] = [uuid(),uuid(),uuid(),uuid()]; 
+            const res = await axios.post("http://localhost:3000/events",basic);
+            const tuple = await onlyDaoCtx.daoProvider.review().selectReviewContent(request.reviewId);
+            expect(tuple?.length).to.equal(4);
         })
+        it("/events mod 액션 comment 내용 수정 Test",async () => {
+
+            const basic = makeBasicRandomEventBody("MOD") as any;
+            basic.reviewId = request.reviewId;
+            basic.placeId = request.placeId;
+            basic.userId = request.userId;
+
+            basic["content"] = "hello you"; 
+            const res = await axios.post("http://localhost:3000/events",basic);
+            const tuple = await onlyDaoCtx.daoProvider.review().selectReview(request.reviewId);
+            expect(tuple?.comment).to.equal("hello you");
+        })
+
         it("모든 테이블 튜플 삭제",async() => {
             await deleteAllData(await conn);
         })
@@ -80,6 +98,22 @@ describe("http 서버 테스트",()=>{
             expect(res.data.length).to.equal(1); 
             const element = res.data[0];
             expect(element.reviewId).to.equal(request.reviewId);
+        });
+
+        describe("Delete 처리 Test",()=> {
+            const request = makeRandomEventBody("hello world to");
+            it("/events Add 처리 Test",async () => {
+                const res = await axios.post("http://localhost:3000/events",request);
+                expect(res.status).to.equal(201);
+                const dbTuple = onlyDaoCtx.daoProvider.review().selectReview(request.reviewId);
+                expect(dbTuple).to.not.equal(null);
+            })
+            it("/events delete 리뷰 삭제 Test",async () => {
+                request.action = "DELETE";
+                await axios.post("http://localhost:3000/events",request);
+                const dbTuple = await onlyDaoCtx.daoProvider.review().selectReview(request.reviewId);
+                expect(dbTuple).to.equal(null);
+            });
         });
         it("모든 테이블 튜플 삭제",async() => {
             await deleteAllData(await conn);
